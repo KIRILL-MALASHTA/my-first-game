23f831a45a8e1c317fdfd5819db1b23e2c6a539a
Kirill <Kirillsergeevich2203@gmail.com>
2024-02-18 19:54:06 +0700

    последние изменения

---
+++ assets/scripts/main.js
@@ -0,0 +1,10 @@
+let scene = new Phaser.Scene("Game");
+
+let config={
+    type: Phaser.AUTO,// webgl or canvas
+    width:1280,
+    height:720,
+    scene: scene
+};
+
+let game=new Phaser.geme(config);
